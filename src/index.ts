267e86c73c7852afab3e8dcbeb5e8da0d6e96a86
mazenfteha <mazn34903@gmail.com>
2023-07-14 23:14:40 -0700

    server and mongo server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello TypeScript with Node.js');
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+require('dotenv').config();
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(cors({
+  credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(5000, () => {
+  console.log('Server running on http://localhost:5000/');
+});
+
+const MONGO_URL = process.env.MONOGO_URL
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => {
+    console.log(error)
+})
