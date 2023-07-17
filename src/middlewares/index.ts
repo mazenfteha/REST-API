abe482ec7f8e01f9dcbeef567d2b184332ad9e58
mazenfteha <mazn34903@gmail.com>
2023-07-17 00:14:56 -0700

    Users controller and routing

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -5,23 +5,41 @@ import { getUserBySessionToken } from '../db/users';
 
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-      const sessionToken = req.cookies['MAZEN-AUTH'];
-  
-      if (!sessionToken) {
-        return res.sendStatus(403);
-      }
+    const sessionToken = req.cookies['MAZEN-AUTH'];
 
-      const existingUser = await getUserBySessionToken(sessionToken);
-  
-      if (!existingUser) {
+    if (!sessionToken) {
         return res.sendStatus(403);
-      }
-  
-      merge(req, { identity: existingUser });
-  
-      return next();
-    } catch (error) {
-      console.log(error);
-      return res.sendStatus(400);
     }
-  }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+        return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+    } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+    }
+}
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params
+        const currentUserId = get(req, 'identity._id') as string;
+        if (!currentUserId) {
+            return res.sendStatus(403);
+        }
+
+        if (currentUserId.toString() !== id) {
+            return res.sendStatus(403);
+        }
+        next()
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
