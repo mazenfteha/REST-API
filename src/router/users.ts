bbf4a1e0969e305bbf65dc80829a872110ece324
mazenfteha <mazn34903@gmail.com>
2023-07-16 23:47:55 -0700

    Authenticated middleware and users conntrollers and routing

---
+++ src/router/users.ts
@@ -0,0 +1,8 @@
+import  express  from "express";
+
+import { getAllUsers } from "../controllers/users";
+import { isAuthenticated } from "../middlewares/index";
+
+export default(router: express.Router) => {
+    router.get('/users',isAuthenticated ,getAllUsers)
+}
